47bfcc3b7be3a61607d2ed216635374d072bd8c7
Ismael <ismaelirc@gmail.com>
2021-06-09 11:02:57 -0300

    JS and CSS files for login/register view page

---
+++ public/js/register/register.js
@@ -0,0 +1,61 @@
+$(function() {
+    
+    
+    $("#account_form").validate({
+        rules: {
+            full_name:{
+                
+                minlength: 3
+            },
+            username:{
+                minlength: 3
+            },
+            password:{
+                required: true
+            },
+            role:{
+                required: true
+            }
+        },
+        submitHandler: function(form) {
+            let data = $(form).serialize();
+           
+            $.ajax({
+                url: $(form).attr('action'),
+                type: "POST",
+                dataType: 'json',
+                data: data,
+                beforeSend: function(){
+                    $.LoadingOverlay("show");
+                }
+            }).done(function(data) {
+                
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Done!',
+                    text: data.success,
+                    footer: '<a href="'+data.login_page+'">Login Page</a>'
+                });
+                
+            }).fail(function(data) {
+                
+                let ul_errors = '';
+                
+                $.each(data.responseJSON.error, function( key, value ) {
+                    ul_errors += value+'<br />';
+                });
+
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...Check the errors bellow!',
+                    html: ul_errors
+                });
+
+            }).always(function(){
+                $.LoadingOverlay("hide");
+                
+            });
+            return;
+        }
+    });
+});
