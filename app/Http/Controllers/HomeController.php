40c10fa909bcd32b00536bf0a0c52031c27c0acc
Ismael <ismaelirc@gmail.com>
2021-06-11 00:13:11 -0300

    Method index created to hangle with the dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use JWTAuth;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Validator;
 
 class HomeController extends Controller
 {
-    //
+    protected $user;
+
+    public function __construct()
+    {
+        $this->user = JWTAuth::parseToken()->authenticate();
+    }
+
+     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home.index');
+        //return $this->user;
+    }
 }
