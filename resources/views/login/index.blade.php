3a9352e3b8a3c3371e0fb8ac6036eaf0f5966332
Ismael <ismaelirc@gmail.com>
2021-06-11 00:37:11 -0300

    change the route and include the js script

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,12 +3,16 @@
 @push('styles')
     <link href="{{ asset('css/form_login_register.css') }}" type="text/css" rel="stylesheet">
 @endpush
+@push('scripts')
+    <script src="{{ asset('js/login/login.js') }}" type="text/javascript"></script>
+@endpush
     <main class="form-signin">
-        <form action="#" name="login-form" id="login-form" method="post">
+        <form action="{{ route('login')}}" name="login_form" id="login_form" method="post">
+            @csrf
             <h1 class="h3 mb-3 fw-normal text-center">Please sign in</h1>
             
             <div class="mb-3">
-                <input type="text" class="form-control" id="username" name="username" placeholder="Your user name"> 
+                <input type="text" class="form-control" id="username" name="username" placeholder="Your username"> 
             </div>
 
             <div class="mb-3">
@@ -16,7 +20,7 @@
             </div>
 
             <div class="d-grid gap-2">
-                <button class="w-100 btn btn-lg btn-primary" type="button">Sign in</button>
+                <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
                 <span>Don't have an account? <a href="{{ route('register')}}" class="text-center">Sign up</a></span>
             </div>
             <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
