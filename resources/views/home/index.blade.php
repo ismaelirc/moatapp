cb128981e59d778120c8c3d0cf3a439d8d0d89bd
Ismael <ismaelirc@gmail.com>
2021-06-11 13:38:34 -0300

    Change the dashboard to new style

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -28,37 +28,13 @@
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">
                             <span data-feather="home"></span>
-                            Dashboard
+                            Artist list
                             </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">
                             <span data-feather="file"></span>
-                            Orders
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                            <span data-feather="shopping-cart"></span>
-                            Products
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                            <span data-feather="users"></span>
-                            Customers
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                            <span data-feather="bar-chart-2"></span>
-                            Reports
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                            <span data-feather="layers"></span>
-                            Integrations
+                            Albuns
                             </a>
                         </li>
                     </ul>
@@ -68,32 +44,6 @@
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2">Dashboard</h1>
-                </div>
-
-                <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
-
-                <h2>Section title</h2>
-                <div class="table-responsive">
-                    <table class="table table-striped table-sm">
-                        <thead>
-                            <tr>
-                            <th>#</th>
-                            <th>Header</th>
-                            <th>Header</th>
-                            <th>Header</th>
-                            <th>Header</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1,001</td>
-                                <td>random</td>
-                                <td>data</td>
-                                <td>placeholder</td>
-                                <td>text</td>    
-                            </tr>
-                        </tbody>
-                    </table>
                 </div>
             </main>
         </div>
