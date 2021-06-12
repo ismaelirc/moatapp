c552c74c8b1685fe367b07743784494d26f70b18
Ismael <ismaelirc@gmail.com>
2021-06-12 11:05:29 -0300

    Create JS and View for albums

---
+++ public/js/album/album.js
@@ -0,0 +1,61 @@
+$(function() {
+    $(function() {
+        $("#year").mask('9999');
+
+        $("#album_form").validate({
+            rules: {
+                artist:{
+                    required: true
+                },
+                album_name:{
+                    required: true,
+                    minlength: 3
+                },
+                year:{
+                    required: true
+                }
+            },
+            submitHandler: function(form) {
+                let data = $(form).serialize();
+                let token = $("#token").val();
+                let action = $(form).attr('action')+'?token='+token;
+                //let album = $("#album").val();
+
+                $.ajax({
+                    url: action,
+                    type: $(form).attr('method'),
+                    dataType: 'json',
+                    data: data,
+                    beforeSend: function(){
+                        $.LoadingOverlay("show");
+                    }
+                }).done(function(data) {
+                   
+                    $("#album").val(data.album_id);
+                    $(form).attr('action','/album/update');
+                    //$(form).attr('method','PUT');
+
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Done!',
+                        text: data.success
+                    });
+
+                }).fail(function(data) {
+                    
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...Check the errors bellow!',
+                        text: data.responseJSON.message
+                    });
+    
+                }).always(function(){
+                    $.LoadingOverlay("hide");
+                    
+                });
+                return;
+            }
+        });
+    });
+
+});
---
+++ resources/views/album/form.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.app')
+@section('content')
+@push('styles')
+    <link href="{{ asset('css/home.css') }}" type="text/css" rel="stylesheet">
+@endpush
+@push('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/album/album.js') }}" type="text/javascript"></script>
+@endpush
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="https://www.moat.ai/">Moat.ai</a>
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+    <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+        <a class="nav-link" href="{{route('logout').'?token='.JWTAuth::getToken()}}">Sign out</a>
+        </li>
+    </ul>
+    </header>
+
+    <div class="container-fluid">
+        <div class="row">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+                <div class="position-sticky pt-3">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="{{route('home').'?token='.JWTAuth::getToken()}}">
+                            <span data-feather="home"></span>
+                            Artist list
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="{{route('album').'?token='.JWTAuth::getToken()}}">
+                            <span data-feather="file"></span>
+                            Albuns
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                    <h1 class="h2">Album</h1>
+                </div>
+                <div class="col-sm-4 col-md-6 col-lg-12">
+                    <input type="hidden" name="token" id="token" value="{{JWTAuth::getToken()}}" />
+                    <form action="{{ route('album.create')}}" name="album_form" id="album_form" method="post">
+                        @csrf
+                        <input type="hidden" name="album" id="album" value="0" />
+                        <div class="mb-3">
+                            <label for="artist" class="form-label">Artist</label>
+                            <select class="form-select" name="artist" id="artist">
+                                @foreach ($artists as $artist)
+                                    <option value="{{$artist['id']}}">{{$artist['name']}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="album_name" class="form-label">Album name</label>
+                            <input type="text" class="form-control" name="album_name" id="album_name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="year" class="form-label">Album Year</label>
+                            <input type="text" class="form-control" name="year" id="year">
+                        </div>
+                        
+                        <button type="submit" class="btn btn-primary">Save album</button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    </div>
+@endsection
