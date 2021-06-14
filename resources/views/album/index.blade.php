7de3f7acff2c60f7124cf010b0d9a639d5bf3a4e
Ismael <ismaelirc@gmail.com>
2021-06-14 11:57:33 -0300

    Buttom to add the first album and warning when no artists list where found

--- resources/views/album/index.blade.php
+++ resources/views/album/index.blade.php
@@ -13,31 +13,42 @@
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2">Albuns list</h1>
                 </div>
-                <div class="col-md-12">
-                    <a href="{{route('album.new').'?token='.JWTAuth::getToken()}}" class="btn btn-primary">New Album</a>
-                </div>
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">ID</th>
-                            <th scope="col">Album name</th>
-                            <th scope="col">Artist name</th>
-                            <th scope="col">Year</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($albums as $album)
-                        <tr>
-                            <td>{{ $album->id }}</td>
-                            <td><a href="{{ route('album.edit',['id' => $album->id, 'token' => JWTAuth::getToken()])}}">{{ $album->album_name }}</a></td>
-                            <td>{{ $album->artist_name }}</td>
-                            <td>{{ $album->year }}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                   
-                </table>
+                
+                @if(count($albums) > 0)
+                    
+                    <div class="col-md-12">
+                        <a href="{{route('album.new').'?token='.JWTAuth::getToken()}}" class="btn btn-primary">New Album</a>
+                    </div>
+                    
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Album name</th>
+                                <th scope="col">Artist name</th>
+                                <th scope="col">Year</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($albums as $album)
+                            <tr>
+                                <td>{{ $album->id }}</td>
+                                <td><a href="{{ route('album.edit',['id' => $album->id, 'token' => JWTAuth::getToken()])}}">{{ $album->album_name }}</a></td>
+                                <td>{{ $album->artist_name }}</td>
+                                <td>{{ $album->year }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>   
 
+                @else
+
+                    <div class="alert alert-primary" role="alert">
+                        Click <a href="{{route('album.new').'?token='.JWTAuth::getToken()}}"><b>here</b></a> to add the first album!
+                    </div>
+                
+                @endif
+                
             </main>
         </div>
     </div>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -13,24 +13,34 @@
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                     <h1 class="h2">Artists list</h1>
                 </div>
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">ID</th>
-                            <th scope="col">Artist</th>
-                            <th scope="col">Twitter</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($artists as $artist)
-                        <tr>
-                            <td>{{ $artist['id'] }}</td>
-                            <td><a href="{{route('album',['token' => JWTAuth::getToken(), 'id' => $artist['id']])}}">{{ $artist['name'] }}</a></td>
-                            <td><a href="https://twitter.com/{{ $artist['twitter'] }}">{{ $artist['twitter'] }}</a></td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                @if(count($artists) > 0)
+
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Artist</th>
+                                <th scope="col">Twitter</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($artists as $artist)
+                            <tr>
+                                <td>{{ $artist['id'] }}</td>
+                                <td><a href="{{route('album',['token' => JWTAuth::getToken(), 'id' => $artist['id']])}}">{{ $artist['name'] }}</a></td>
+                                <td><a href="https://twitter.com/{{ $artist['twitter'] }}">{{ $artist['twitter'] }}</a></td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>    
+
+                @else
+
+                    <div class="alert alert-danger" role="alert">
+                        Artist list not found. Please contact support!
+                    </div>  
+                      
+                @endif
 
             </main>
         </div>
